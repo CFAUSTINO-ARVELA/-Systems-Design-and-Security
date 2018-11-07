f44b357da72a85253a732f411abcfa68acf088f5
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2018-11-07 11:08:30 +0000

    Changed lecturer to tutor

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@ class Student {
 
     private String degree; // needs to be converted to type Degree when created
     private int registrationNumber;
-    private String lecturer; // needs to be type Teacher
+    private String tutor; // needs to be type Teacher
 
     Student() {
 
