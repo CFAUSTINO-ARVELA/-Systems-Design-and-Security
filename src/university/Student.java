a2328bf3acf692d8ff429e5ab170132ac9b19f37
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2018-11-19 10:23:10 +0000

    Wrote create student method

--- src/university/Student.java
+++ src/university/Student.java
@@ -1,5 +1,10 @@
 package university;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 class Student {
 
     private String degree; // needs to be converted to type Degree when created
@@ -15,6 +20,31 @@ class Student {
       this.tutor = tut;
       this.accountDetails = acc;
     }
+    
+	public Student createStudent() throws SQLException {
+
+		Connection con = null;
+		Statement stmt = null;
+
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team002", "team002", "e8f208af");
+			stmt = con.createStatement();
+			String query = String.format("INSERT INTO student (registrationNumber, degree, tutor, username) VALUES (\"%d\", \"%s\", \"%s\", \"%s\");",
+					this.registrationNumber, this.degree, this.tutor, this.accountDetails.getUsername());
+			int count = stmt.executeUpdate(query);
+					
+
+			System.out.println(count);
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (stmt != null)
+				stmt.close();
+		}
+		
+		return this;
+
+	}
 
     public String getDegree() {
         return this.degree;
