53aa8cf4d523c2e156fe5f23da98054700067f6f
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2018-11-21 10:34:05 +0000

    Wrote basic module choice class

---
+++ src/university/ModuleChoice.java
@@ -0,0 +1,23 @@
+package university;
+
+public class ModuleChoice {
+	
+	private int registrationNumber;
+	private String moduleCode;
+	private char period;
+	private int grade;
+	
+	public int getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public String getModuleCode() {
+		return moduleCode;
+	}
+	public char getPeriod() {
+		return period;
+	}
+	public int getGrade() {
+		return grade;
+	}
+
+}
