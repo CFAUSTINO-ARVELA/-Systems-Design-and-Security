e49aa7d0e689c32a55dc47f2df3c56c349fbc405
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2018-11-23 14:50:21 +0000

    Added a module grade class for ease of writing progression function

---
+++ src/university/ModuleGrades.java
@@ -0,0 +1,21 @@
+package university;
+
+public class ModuleGrades {
+	
+	private String moduleCode;
+	private int grade;
+	private int resitGrade;
+	
+	public String getModuleCode() {
+		return moduleCode;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public int getResitGrade() {
+		return resitGrade;
+	}
+	
+	
+
+}
