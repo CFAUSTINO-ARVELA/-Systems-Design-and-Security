f83e9c56a3aa99a655cc40314c11a46ebb51e004
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2018-11-27 12:17:33 +0000

    Fixed period result issue

--- src/university/Degree.java
+++ src/university/Degree.java
@@ -377,7 +377,7 @@ public class Degree{
 			ArrayList<ArrayList<String>> modList = new ArrayList<ArrayList<String>>();
 			ArrayList<String> mod = new ArrayList<String>();
 			ResultSet res = null;
-			PreparedStatement getMod = snull;
+			PreparedStatement getMod = null;
 			connectToDB();
 			getMod = con.prepareStatement("SELECT modCode, name, mandatory,year,duration, credits  FROM assoModDeg JOIN module WHERE assoModDeg.modCode = module.code AND degCode = ?;");
 			try {
